1f0b366ece853bae4ac7d5a12a942c03b2190599
Mikhael Gaster <mikhaelgaster@gmail.com>
2022-09-17 19:50:12 -0700

    initial_commit

---
+++ scraping/scrape_reddit/functions.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+def analyze_comment_sentiment(comment:str) -> float:
+    # https://huggingface.co/blog/sentiment-analysis-python
+    # https://huggingface.co/finiteautomata/bertweet-base-sentiment-analysis?text=what+is+sonny%27s+problem
+    # https://huggingface.co/cardiffnlp/twitter-roberta-base-sentiment
+    pass
+
+def scrape_comments_from_post(post_url:str) -> pd.DataFrame:
+    pass
+
